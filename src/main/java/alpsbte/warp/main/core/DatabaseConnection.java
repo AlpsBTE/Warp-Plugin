7a4599e771e1ec3954889f14ef6b92538b1ba35a
cinnazeyy <imcinnazeyy@gmail.com>
2021-11-15 20:50:49 +0100

    added DatabaseConnection

---
+++ src/main/java/alpsbte/warp/main/core/DatabaseConnection.java
@@ -0,0 +1,97 @@
+package alpsbte.warp.main.core;
+
+import alpsbte.warp.main.Main;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
+
+public class DatabaseConnection {
+    private final static HikariConfig config = new HikariConfig();
+    private static HikariDataSource dataSource;
+
+    public static void InitializeDatabase() throws ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        FileConfiguration configFile = Main.getPlugin().getConfig();
+        String URL = configFile.getString("database.url");
+        String name = configFile.getString("database.name");
+        String username = configFile.getString("database.username");
+        String password = configFile.getString("database.password");
+
+        config.setJdbcUrl(URL + name);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    @Deprecated
+    public static Connection getConnection() {
+        int retries = 3;
+        while (retries > 0) {
+            try {
+                return dataSource.getConnection();
+            } catch (SQLException ex) {
+                Bukkit.getLogger().log(Level.SEVERE, "Database connection failed!\n\n" + ex.getMessage());
+            }
+            retries--;
+        }
+        return null;
+    }
+
+    public static StatementBuilder createStatement(String sql) {
+        return new StatementBuilder(sql);
+    }
+
+    public static class StatementBuilder {
+        private final String sql;
+        private final List<Object> values = new ArrayList<>();
+
+        public StatementBuilder(String sql) {
+            this.sql = sql;
+        }
+
+        public StatementBuilder setValue(Object value) {
+            values.add(value);
+            return this;
+        }
+
+        public ResultSet executeQuery() throws SQLException {
+            try (Connection con = dataSource.getConnection()) {
+                try (PreparedStatement ps = Objects.requireNonNull(con).prepareStatement(sql)) {
+                    return iterateValues(ps).executeQuery();
+                }
+            }
+        }
+
+        public void executeUpdate() throws SQLException {
+            try (Connection con = dataSource.getConnection()) {
+                try (PreparedStatement ps = Objects.requireNonNull(con).prepareStatement(sql)) {
+                    iterateValues(ps).executeUpdate();
+                }
+            }
+        }
+
+        private PreparedStatement iterateValues(PreparedStatement ps) throws SQLException {
+            for (int i = 0; i < values.size(); i++) {
+                ps.setObject(i + 1, values.get(i));
+            }
+            return ps;
+        }
+    }
+}
+
+
